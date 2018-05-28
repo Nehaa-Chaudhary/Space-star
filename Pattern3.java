20546cd685dc8a9ea68f20a5e2ff0907f0c13210
Neha Chaudhary <nehachaudhary875@gmail.com>
2018-05-28 23:48:58 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BEAUTIFUL NEHA
+ */
+public class Pattern3 {
+    void spaceStar()
+    {
+        for(int i=1;i<=5;i++) 
+    {
+        for(int j=5;j>=i;j--)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+       /* for(int i=1;i<=4;i++)
+        {
+            for(int j=1;j<=i;j++)
+        
+                {
+                    System.out.print("*");
+                }
+            System.out.println();
+        }
+    }*/
+public static void main(String a[])
+{
+    Pattern3 p1=new Pattern3();
+            p1.spaceStar();
+}
+}
